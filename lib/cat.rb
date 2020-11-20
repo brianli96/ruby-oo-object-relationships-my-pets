adb7ef33e4b5cadb4103656a3db544301ce69c27
Brian Li <Brian.Li96@yahoo.com>
2020-11-19 23:33:01 -0600

    commit tree ab34642b369c474b248a4534aa3baa025ee9e4f1

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,18 @@
 class Cat
-  # code goes here
+  attr_accessor :owner, :mood
+  attr_reader :name
+
+  @@cats = []
+
+  def self.all
+    @@cats
+  end
+
+  def initialize(name, owner)
+    @name = name
+    @owner = owner
+    @mood = "nervous"
+    @@cats << self
+  end
+
 end
